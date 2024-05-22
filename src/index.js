d248c93464a07698e309d0031f36e51b95837a91
BigJoeDev <Josephiibanga3@gmail.com>
2024-05-22 14:31:31 -0700

    learningwithoutfred

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+
+const app = express();
+
+//use ejs as the view engine
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+
+
+ 
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('server is running on port 3000');
+
+})
