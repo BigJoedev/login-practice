e8f38d815cd60d4450f52e43db94fa0130d72ed5
BigJoeDev <Josephiibanga3@gmail.com>
2024-05-28 10:31:50 -0700

    fixin login errors2

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 
 const express = require('express');
-const path = require("path");
+const path = require('path');
 const bcrypt = require("bcrypt");
 
 
 const app = express();
 
 //use ejs as the view engine
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("login");
+app.get('/', (req, res) => {
+    res.render('login');
 });
 
-app.get("/signup", (req, res) => {
-    res.render("signup");
+app.get('/signup', (req, res) => {
+    res.render('signup');
 });
 
 
@@ -28,4 +31,4 @@ const port = 3000;
 app.listen(port, () => {
     console.log('server is running on port 3000');
 
-})
+});
